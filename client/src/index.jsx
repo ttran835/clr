6ca24a0bd8cad0e4386baa2258cb342403068189
Tim Tran <timttran835@gmail.com>
2020-04-12 13:43:24 -0700

    v.1.0.0 initial commit

---
+++ client/src/index.jsx
@@ -0,0 +1,15 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Apps from './Components/Apps.jsx';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Apps />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
---
+++ server/index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const router = require('./routes/routes');
+
+const app = express();
+// const db = require('./database');
+
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https' || req.headers.host === 'localhost:3000') {
+    return next();
+  }
+  res.redirect('https://' + req.hostname + req.url);
+});
+
+app.use(helmet());
+app.use(
+  helmet.hsts({
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true,
+  }),
+);
+
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'public, max-age=31557600, s-maxage=31557600');
+  next();
+});
+
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.use('/api', router);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Port is ${port}`);
+});
