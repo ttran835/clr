cdf209f9031fe7a99e4aa4952d2b550e317cc7b4
Tim Tran <timttran835@gmail.com>
2020-04-12 20:22:08 -0700

    v.1.0.1 set up database, connected to heroku and update domain to include ssl

---
+++ database/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const { Op } = Sequelize;
+const operatorsAliases = {
+  $like: Op.like,
+};
+const devDb = new Sequelize(
+  process.env.LOCAL_DB,
+  process.env.LOCAL_USER,
+  process.env.LOCAL_PASSWORD,
+
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false,
+    },
+    operatorsAliases: { operatorsAliases },
+  },
+);
+
+const productionDb = new Sequelize(process.env.HEROKU_DB, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true,
+  },
+  operatorsAliases: { operatorsAliases },
+});
+
+const sequelize = process.env.LOCAL_DB === undefined ? productionDb : devDb;
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Conected to DB'))
+  .catch((err) => console.error(err));
+sequelize
+  .sync()
+  .then(() => console.log('App synced with all tables.'))
+  .catch((err) => console.error(err));
+
+module.exports = { sequelize };
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const router = require('./routes/routes');
 
 const app = express();
-// const db = require('./database');
+const db = require('../database');
 
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
